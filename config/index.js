a1e4bfb27a2e6a4e3e6366f473c0558599b47160
Leslie991226 <1448710303@qq.com>
2021-11-07 16:55:20 +0800

    change router

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-todolist/',
+    assetsPublicPath: './vue-todolist',
     proxyTable: {},
 
     // Various Dev Server settings
